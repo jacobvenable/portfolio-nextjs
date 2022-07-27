9ad75b9ffe28d219e6d83aabd573a2f9136fe47e
jacobvenable <jacob.venable@gmail.com>
2022-07-27 07:54:16 -0600

    style: add global styles to app

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { AppProps } from "next/app";
+
+import "../styles/global.scss";
+
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+export default App;
