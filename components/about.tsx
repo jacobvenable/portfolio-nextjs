874619b1b67eb90f615cb821c1b9a1e0a544af87
jacobvenable <jacob.venable@gmail.com>
2022-07-27 19:25:04 -0600

    fix: commit missed about component

---
+++ components/about.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+import "./about.module.scss";
+
+const About = () => (
+  <div className="about">
+    <div className="about__intro">
+      <h1 className="sr-only">Jacob Venable</h1>
+      <p className="about__greeting">Hello,</p>
+      <p>
+        I&apos;m Jacob, a front-end web developer. I&apos;ve been developing
+        since high school, but found a love for the practicality and reach of
+        the web.
+      </p>
+      <p>
+        Currently, I&apos;m a senior front-end web developer at Purdue
+        University&apos;s Office of Marketing and Media. Boiler Up!
+      </p>
+      <div className="container__row">
+        <div className="container__column">
+          <Link
+            href="/work"
+            className="button button--yellow-light about__button"
+          >
+            View my Work
+          </Link>
+        </div>
+        <div className="container__column">
+          <Link
+            href="/contact"
+            className="button button--blue-dark-reverse about__button"
+          >
+            Contact Me
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
