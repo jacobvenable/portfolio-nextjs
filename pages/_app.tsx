a750e860131b03d0173f581e95abea14d1ff8292
jacobvenable <jacob.venable@gmail.com>
2022-07-29 07:26:14 -0600

    feat: add fontawesome icon loading to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 import "../styles-global/index.scss";
 
