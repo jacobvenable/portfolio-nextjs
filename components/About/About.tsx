721c90137bad075e066d82d1c7270fc0cbd1e19d
jacobvenable <jacob.venable@gmail.com>
2022-07-31 15:35:55 -0600

    refactor: fix all stylelint rules

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,12 +1,13 @@
 import Link from "components/Link";
 
 import styles from "./About.module.scss";
+console.log("styles: ", styles);
 
 const About = () => (
   <div className={styles.containerOuter}>
     <div className={styles.containerInner}>
       <h1 className="sr-only">Jacob Venable</h1>
-      <p className="heading-2">Hello,</p>
+      <p className={`heading-2 ${styles.heading}`}>Hello,</p>
       <p>
         I&apos;m Jacob, a front-end web developer. I&apos;ve been developing
         since high school, but found a love for the practicality and reach of
@@ -20,7 +21,7 @@ const About = () => (
         <div className="container__column">
           <Link
             href="/work"
-            className="button button--yellow-light about__button"
+            className={`button button--yellow-light ${styles.button}`}
           >
             View my Work
           </Link>
@@ -28,7 +29,7 @@ const About = () => (
         <div className="container__column">
           <Link
             href="/contact"
-            className="button button--blue-dark-reverse about__button"
+            className={`button button--blue-dark-reverse ${styles.button}`}
           >
             Contact Me
           </Link>
