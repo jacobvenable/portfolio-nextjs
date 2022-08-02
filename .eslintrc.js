ab2d9192950e1a02c116a6b145f42803f06c29b4
jacobvenable <jacob.venable@gmail.com>
2022-08-01 19:16:50 -0600

    chore: add lint staged and husky integration with eslint, tsc, and stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+  ],
+  ignorePatterns: [".eslintrc.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+};
