3141e1ee570045bb326c4bc381f0400f3adbf83e
jacobvenable <jacob.venable@gmail.com>
2022-08-06 08:50:36 -0600

    type: fix svg module type

---
+++ custom-next-env.d.ts
@@ -0,0 +1,3 @@
+// overriding next-env.d to solve issues with importing SVGs
+
+/// <reference types="next" />
--- images/index.d.ts
+++ images/index.d.ts
@@ -1,4 +1,22 @@
+/*
+  Explicitly declare module for files in images folder to fix
+  type problems with importing SVGs. Current implementation 
+  assumes all images contain only static image data. This may 
+  need updated to support inline SVGs.
+*/
+
+type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  placeholder?: string;
+};
+
+declare module "*.png" {
+  const content: StaticImageData;
+  export default content;
+}
 declare module "*.svg" {
-  const value: string;
-  export = value;
+  const content: StaticImageData;
+  export default content;
 }
