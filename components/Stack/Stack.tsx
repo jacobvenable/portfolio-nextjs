391f79ff4f8c9a25fb613358b66984d2754e4358
jacobvenable <jacob.venable@gmail.com>
2022-08-06 16:23:19 -0600

    refactor: Stack styles

--- components/Stack/Stack.tsx
+++ components/Stack/Stack.tsx
@@ -2,13 +2,15 @@ import styles from "./Stack.module.scss";
 
 interface Props {
   children: React.ReactNode[];
+  padded?: boolean;
 }
 
-const Stack: React.FC<Props> = ({ children }) => {
+const Stack: React.FC<Props> = ({ children, padded }) => {
+  const paddedClassName = padded ? "padded" : "";
   return (
-    <div className={styles.stack}>
+    <div className={`${styles.stack} ${paddedClassName}`}>
       {children.map((child, index) => (
-        <div className={styles.item} key={index}>
+        <div className={`${styles.item} ${paddedClassName}`} key={index}>
           {child}
         </div>
       ))}
