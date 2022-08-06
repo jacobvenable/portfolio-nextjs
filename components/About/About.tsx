a146160ba4b5f154a6ffabe2dae928715583ffe8
jacobvenable <jacob.venable@gmail.com>
2022-08-06 15:59:44 -0600

    feat: implement typography component in about content

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,21 +2,26 @@ import styles from "./About.module.scss";
 import Button from "components/Button";
 import Link, { LinkProps } from "components/Link";
 import Stack from "components/Stack";
+import Typography from "components/Typography";
 
 const About = () => (
   <div className={styles.containerOuter}>
     <div className={styles.containerInner}>
-      <h1 className="sr-only">Jacob Venable</h1>
-      <p className={`heading-2 ${styles.heading}`}>Hello,</p>
-      <p>
+      <Typography component="h1" variant="sr-only">
+        Jacob Venable
+      </Typography>
+      <Typography className={styles.heading} component="p" variant="h2">
+        Hello,
+      </Typography>
+      <Typography>
         I&apos;m Jacob, a front-end web developer. I&apos;ve been developing
         since high school, but found a love for the practicality and reach of
         the web.
-      </p>
-      <p>
+      </Typography>
+      <Typography>
         Currently, I&apos;m a senior front-end web developer at Purdue
         University&apos;s Office of Marketing and Media. Boiler Up!
-      </p>
+      </Typography>
       <Stack>
         <Button<LinkProps> color="yellow-light" component={Link} href="/work">
           View my Work
