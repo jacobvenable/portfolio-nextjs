9301ed8599d3b0ea26f4e66b144abadb874f8350
jacobvenable <jacob.venable@gmail.com>
2022-08-07 16:37:40 -0600

    feat: add direction and itemProps props to Stack

--- components/Stack/Stack.tsx
+++ components/Stack/Stack.tsx
@@ -2,15 +2,26 @@ import styles from "./Stack.module.scss";
 
 interface Props {
   children: React.ReactNode[];
+  direction?: "horizontal" | "vertical";
+  itemProps?: React.HTMLProps<HTMLDivElement>;
   padded?: boolean;
 }
 
-const Stack: React.FC<Props> = ({ children, padded }) => {
-  const paddedClassName = padded ? "padded" : "";
+const Stack: React.FC<Props> = ({
+  children,
+  direction = "horizontal",
+  itemProps: { className: itemClassName = "", ...itemProps } = {},
+  padded,
+}) => {
+  const paddedClassName = padded ? styles.padded : "";
   return (
-    <div className={`${styles.stack} ${paddedClassName}`}>
+    <div className={`${styles.stack} ${styles[direction]} ${paddedClassName}`}>
       {children.map((child, index) => (
-        <div className={`${styles.item} ${paddedClassName}`} key={index}>
+        <div
+          className={`${styles.item} ${styles[direction]} ${paddedClassName} ${itemClassName}`}
+          key={index}
+          {...itemProps}
+        >
           {child}
         </div>
       ))}
