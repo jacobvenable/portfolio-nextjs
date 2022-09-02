696aa4ee87bfecd9949200ba7be78144c3d60b90
jacobvenable <jacob.venable@gmail.com>
2022-09-01 19:34:18 -0600

    feat: add copy to contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,60 @@
-const ContactPage = () => <h1>Contact Page Placeholder</h1>;
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Button from "components/Button";
+import ContentContainer from "components/ContentContainer";
+import Grid from "components/Grid";
+import Link, { LinkProps } from "components/Link";
+import Stack from "components/Stack";
+import Typography from "components/Typography";
+
+const ContactPage = () => {
+  return (
+    <ContentContainer>
+      <Typography variant="h1">Want to talk?</Typography>
+      <Grid.Container>
+        <Grid.Item medium={35}>
+          <Typography>
+            Have an idea for a project, or want to discuss a new opportunity?
+            Let&apos;s have a chat!
+          </Typography>
+          <Typography>
+            You can use the form to send me a direct message or connect with me
+            through any of the networks below.
+          </Typography>
+          <Stack padded>
+            <Button<LinkProps>
+              color="yellow-light"
+              component={Link}
+              href="https://www.linkedin.com/in/jacob-venable/"
+              size="large"
+              variant="ghost"
+            >
+              <Typography variant="sr-only">
+                Jacob Venable LinkedIn profile
+              </Typography>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Button>
+            <Button<LinkProps>
+              color="yellow-light"
+              component={Link}
+              href="https://github.com/jacobvenable"
+              size="large"
+              variant="ghost"
+            >
+              <Typography variant="sr-only">
+                Jacob Venable GitHub profile
+              </Typography>
+              <FontAwesomeIcon icon={faGithub} />
+            </Button>
+          </Stack>
+        </Grid.Item>
+        <Grid.Item medium={65}>
+          <></>
+        </Grid.Item>
+      </Grid.Container>
+    </ContentContainer>
+  );
+};
 
 export default ContactPage;
